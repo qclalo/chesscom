ebcf9879166826ae098be723a012cbd8cd470f3b
qclalo <bbeard809@gmail.com>
2023-09-04 12:26:36 -0400

    a

--- coordinate.py
+++ coordinate.py
@@ -6,3 +6,6 @@ def on_move(x, y):
 # Create a listener that calls the on_move function when the mouse is moved
 with Listener(on_move=on_move) as listener:
     listener.join()
+
+# 320, 175 --------------- 1070,175
+# 320, 925 --------------- 1070,925
